4f69c96d7671daadafdcbe62b45fe1fcd2962570
lakshman.avula@outlook.com <lakshman.avula@outlook.com>
2019-07-30 13:30:45 +0530

    project is added

--- project/app.py
+++ project/app.py
@@ -1,45 +1,129 @@
 import database as db
 
 def c_add_internship():
-    
     iname = input("Internship name:")
-    cname = input("Company name:")
-    year =  int(input("Conducted year:"))
-    db.add_internship(iname, cname, year)
+    company = input("Company name:")
+    i_year = int(input("Conducted year:"))
+    db.add_internship(iname, company, i_year)
 
 def c_view_all_internships():
-    pass
-
+    db.view_all_internships()
+    
 def c_search_internship_by_name():
-    pass
-
+    name = input("Enter the name to search:")
+    db.search_internship_by_name(name)
 def c_change_status_internship():
     pass
-
 def c_delete_internship():
+    pass
+
+# Student oprations
+def c_add_student():
+    pass
+def c_view_all_student():
+    pass
+def c_search_student_by_name():
+    pass
+def c_update_student():
+    pass
+def c_delete_student():
+    pass
+
+# Registrations and summary reports 
+def c_company_ws_count():
+    pass
+def c_student_ws_count():
+    pass
+def c_ws_student_reports():
+    pass
+
+def c_reg_stu_internship():
+    pass
+
+def c_update_stu_intership_status():
     pass
 
 
 while True:
-    print("*"*50)
-    print("Internship programms conducted at MITE by Companies")
-    print("1.Add 2.View All 3.Search 4.Update 5.Delete 6.Exit")
-    print("*"*50)
     try:
-        ch = int(input("Enter your choice:"))
-        if ch == 1:
-            c_add_internship()
-        elif ch == 2:
-            c_view_all_internships()
-        elif ch == 3:
-            c_search_internship_by_name()
-        elif ch == 4:
-            c_change_status_internship()
-        elif ch == 5:
-            c_delete_internship()
-        elif ch == 6:
+        print("1. Add Internship 2.Student 3.Reports 4.Exit")
+        mch = int(input("Enter you choice:"))
+        
+        if mch == 1:
+            while True:
+                try:
+                    print("*"*50)
+                    print("Internship programms conducted at MITE by Companies")
+                    print("1.Add 2.View All 3.Search 4.Update 5.Delete 6.Main Menu")
+                    print("*"*50)
+                    ch = int(input("Enter your choice:"))
+                    if ch == 1:
+                        c_add_internship()
+                    elif ch == 2:
+                        c_view_all_internships()
+                    elif ch == 3:
+                        c_search_internship_by_name()
+                    elif ch == 4:
+                        c_change_status_internship()
+                    elif ch == 5:
+                        c_delete_internship()
+                    elif ch == 6:
+                        break
+                    else:
+                        raise Exception()
+                except Exception as e:
+                    print("Please provide valid input 1 - 6")
+        elif mch == 2:
+            while True:
+                try:
+                    print("*"*50)
+                    print("Add Students to Internship programms")
+                    print("1.Add 2.View All 3.Search 4.Update 5.Delete 6.Main Menu")
+                    print("*"*50)
+                    ch = int(input("Enter your choice:"))
+                    if ch == 1:
+                        c_add_student()
+                    elif ch == 2:
+                        c_view_all_student()
+                    elif ch == 3:
+                        c_search_student_by_name()
+                    elif ch == 4:
+                        c_update_student()
+                    elif ch == 5:
+                        c_delete_student()
+                    elif ch == 6:
+                        break
+                    else:
+                        raise Exception()
+                except Exception as e:
+                    print("Please provide valid input 1 - 6")
+        elif mch == 3:
+                while True:
+                    try:
+                        print("*"*50)
+                        print("Internship programms conducted at MITE Reports")
+                        print("1.Register Student 2.Update Status 3.Company ws count 4.Student ws count 5.Studen_ws_name_company_year 6.Main Menu")
+                        ch = int(input("Enter your choice:"))
+                        if ch == 1:
+                            c_reg_stu_internship()
+                        elif ch == 2:
+                            c_update_stu_intership_status()                       
+                        if ch == 3:
+                            c_company_ws_count()
+                        elif ch == 4:
+                            c_student_ws_count()
+                        elif ch == 5:
+                            c_ws_student_reports()
+                        elif ch == 6:
+                            break
+                        else:
+                            raise Exception()
+                    except Exception as e:
+                        print("Please provide valid input 1 - 4")
+        elif mch == 4:
+            print("Thank you programming is going to terminate.....")
             exit()
         else:
-            raise TypeError(f"Input value should be <= 6, it was :{ch}")
-    except TypeError as e:
-        print(f"{str(e)}")
+            raise Exception()
+    except Exception as e:
+        print("Enter valid input (1 to 3) only...")
--- project/database.py
+++ project/database.py
@@ -1,16 +1,34 @@
 import time
 import random as rn
+import dbcontext as db
+from models import Internship 
+from beautifultable import BeautifulTable
 
-i_list = []
+def _get_new_id():
+    t_obj = time.localtime()
+    new_id = rn.randint(1000,9900) + (t_obj.tm_min + t_obj.tm_hour + t_obj.tm_sec)
+    return new_id
 
-def add_internship(iname,company,year):
+def add_internship(iname,company,i_year):
     id = _get_new_id()
-    inter_obj = {"id":id,"iname":iname, "company":company, "year":year,"status":1}
-    i_list.append(inter_obj)
-    print(f"Intership programm is added with id: {id} successfully...")
+    try:
+        with db.DbContext() as connection:
+            cursor = connection.cursor()
+            cursor.execute("insert into internship(id,iname,company,i_year,status) values(?,?,?,?,?)",(id,iname,company,i_year,1))
+            print(f"Internship is added successfully with id:{id}")
+    except Exception as e:
+        print(str(e))
 
 def view_all_internships():
-    pass
+    try:
+        with db.DbContext() as connection:
+            cursor = connection.cursor()
+            cursor.execute("select id,iname,company,i_year,status from internship")
+            rows = cursor.fetchall()
+            intern_pro_lst = [Internship(*row) for row in rows]
+            _view_internship_list(intern_pro_lst)
+    except Exception as e:
+        print(str(e))
 def search_internship_by_name(name):
     pass
 def change_status_internship(name):
@@ -18,8 +36,39 @@ def change_status_internship(name):
 def delete_internship():
     pass
 
-def _get_new_id():
-    t_obj = time.localtime()
-    new_id = rn.randint(1000,9900) + (t_obj.tm_min + t_obj.tm_hour + t_obj.tm_sec)
-    return new_id
-    
+def add_student_internship(name,sem,iid):
+    pass
+def view_all_reg_student():
+    pass
+def search_student_by_name(name):
+    pass
+def update_student(sid,sem):
+    pass
+def delete_student(sid):
+    pass
+
+
+def company_ws_count():
+    pass
+def student_ws_count():
+    pass
+def ws_student_reports():
+    pass
+
+def reg_stu_internship():
+    pass
+
+def update_stu_intership_status():
+    pass
+
+
+def _view_internship_list(lst):
+    if lst and len(lst) > 0:
+        table = BeautifulTable()
+        table.column_headers = ["ID", "NAME", "COMPANAY", "YEAR","STATUS"]
+        for l in lst:
+            status = "Comleted" if l.status == 0 else "Going on" 
+            table.append_row([l.id, l.iname, l.company, l.i_year,status])
+        print(table)
+    else:
+        print(f"There are no Intership programms, yet to add...")
