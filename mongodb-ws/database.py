3d9c9d7a3246e7fea3cbd26e57e7a64c5c3784a7
lakshman.avula@outlook.com <lakshman.avula@outlook.com>
2019-08-06 13:13:31 +0530

    mongo project is added

---
+++ mongodb-ws/database.py
@@ -0,0 +1,17 @@
+def add_new_faculty():
+    pass
+def add_subject_to_faculty():
+    pass
+def remove_subject_from_faculty():
+    pass
+def increment_exp_by_one_year():
+    pass
+def update_qualification():
+    pass
+def total_duration_by_faculty():
+    pass
+def subject_with_faclty_name():
+    pass
+def delete_faculty():
+    pass
+
